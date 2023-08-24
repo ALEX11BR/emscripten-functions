b0477ee033e736e4f7e4edb67d7c842dd1768969
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2023-08-24 08:17:14 +0300

    only use the functions for emscripten

--- emscripten-functions/src/lib.rs
+++ emscripten-functions/src/lib.rs
@@ -1,2 +1,4 @@
+#![cfg(target_os = "emscripten")]
+
 pub mod emscripten;
 pub mod console;
--- emscripten-sys/src/lib.rs
+++ emscripten-sys/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg(target_os = "emscripten")]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
